8fe6a5af99d711af1908d8b0e008b21b2fc6f56f
Student <Student@DESKTOP-U98QRUJ>
2016-10-25 15:19:30 +0200

    .

---
+++ Szkolenie JAVA objektowo/src/Osoba.java
@@ -0,0 +1,11 @@
+
+public class Osoba {
+	String imie, nazwisko;
+	int wiek;
+	boolean pelnoletnia() {
+		return wiek>= 18;
+	}
+
+}
+
+
---
+++ Szkolenie JAVA objektowo/src/OsobaMain.java
@@ -0,0 +1,20 @@
+
+public class OsobaMain {
+
+	public static void main(String[] args) {
+		Osoba tato = new Osoba();
+		tato.imie = "Jan";
+		tato.nazwisko = "Kowalski";
+		tato.wiek = 35;
+
+		Osoba corka = new Osoba();
+		corka.imie = "Alicja";
+		corka.nazwisko = "Kowalska";
+		corka.wiek = 10;
+		
+		System.out.println(tato.pelnoletnia());
+		System.out.println(corka.pelnoletnia());
+
+	}
+
+}
