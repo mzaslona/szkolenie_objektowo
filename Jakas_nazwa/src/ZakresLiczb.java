9f87998c93ab46d7c9e4c0d3208dcf2141249e42
Student <Student@DESKTOP-U98QRUJ>
2016-10-25 12:08:01 +0200

    Cwiczenie 4 pokazujace zakres typów danych

---
+++ Jakas_nazwa/src/ZakresLiczb.java
@@ -0,0 +1,20 @@
+
+public class ZakresLiczb {
+
+	public static void main(String[] args) {
+		byte a = 120;
+		a += 20;
+		
+//		byte b+= 20;
+		short s = -32000; s -= 1000;
+		int i = 2000000000; i =2*i;
+		char c = 50;
+		int j = '9';
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(c);
+		System.out.println(j);
+	}
+
+}
