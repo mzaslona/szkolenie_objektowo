9d3952106f4a65219de4cd8430a36c55b4f29e6c
Student <Student@DESKTOP-U98QRUJ>
2016-10-25 16:18:43 +0200

    drobne zmiany

--- Szkolenie JAVA objektowo/src/Osoba.java
+++ Szkolenie JAVA objektowo/src/Osoba.java
@@ -2,8 +2,12 @@
 public class Osoba {
 	String imie, nazwisko;
 	int wiek;
+	int wzrost;
 	boolean pelnoletnia() {
 		return wiek>= 18;
+	}
+	boolean gigant(){
+		return wzrost >180;
 	}
 // c
 }
--- Szkolenie JAVA objektowo/src/OsobaMain.java
+++ Szkolenie JAVA objektowo/src/OsobaMain.java
@@ -6,15 +6,18 @@ public class OsobaMain {
 		tato.imie = "Jan";
 		tato.nazwisko = "Kowalski";
 		tato.wiek = 35;
+		tato.wzrost =150;
 
 		Osoba corka = new Osoba();
 		corka.imie = "Alicja";
 		corka.nazwisko = "Kowalska";
 		corka.wiek = 10;
+		corka.wzrost = 200;
 		
 		System.out.println(tato.pelnoletnia());
 		System.out.println(corka.pelnoletnia());
-
+		System.out.println(tato.gigant());
+		System.out.println(corka.gigant());
 	}
 // c
 }
