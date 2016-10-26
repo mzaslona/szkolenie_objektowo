02e532500af6a3eb4e59bd005dcc1c82069a4f38
Student <Student@DESKTOP-U98QRUJ>
2016-10-26 16:42:40 +0200

    nowe klasy

--- Szkolenie JAVA objektowo/src/Osoba.java
+++ Szkolenie JAVA objektowo/src/Osoba.java
@@ -1,13 +1,12 @@
 
-public class Osoba {
+public abstract class Osoba {
 	String imie, nazwisko;
 	int wiek;
 	int wzrost;
 	private int waga;
-	
-	
-	
-	public Osoba(String imie, String nazwisko, int wiek, int wzrost, int waga) {
+	Plec plec;
+
+	public Osoba(String imie, String nazwisko, int wiek, int wzrost, int waga, Plec plec) {
 		super();
 		this.imie = imie;
 		this.nazwisko = nazwisko;
@@ -15,19 +14,17 @@ public class Osoba {
 		this.wzrost = wzrost;
 		this.waga = waga;
 	}
-	
-	
+
 	boolean pelnoletnia() {
-		return wiek>= 18;
+		return wiek >= 18;
 	}
-	boolean gigant(){
-		return wzrost >180;
+
+	boolean gigant() {
+		return wzrost > 180;
 	}
-	
-	int pobierzWagê(){
+
+	int pobierzWagê() {
 		return waga;
 	}
-// c
+	// c
 }
-
-
--- Szkolenie JAVA objektowo/src/OsobaMain.java
+++ Szkolenie JAVA objektowo/src/OsobaMain.java
@@ -4,33 +4,34 @@ public class OsobaMain {
 
 	public static void main(String[] args) {
 
-		Osoba mama = new Osoba("Halina", "Kowalska", 34, 150, 70);
-		Osoba tato = new Osoba("Jan", "Kowalski", 35, 180, 120);
-		Osoba corka = new Osoba("Bo¿ena", "Kowalska", 5, 120, 20);
-		Osoba syn = new Osoba("W³adzio", "Kowalski", 25, 210, 160);
+		Kobieta mama = new Kobieta("Halina", "Kowalska", 34, 150, 70);
+		Men tato = new Men("Jan", "Kowalski", 35, 210, 210);
+		Osoba corka = new Kobieta("Bo¿ena", "Kowalska", 5, 120, 20);
+		Osoba syn = new Men("W³adzio", "Kowalski", 25, 210, 160);
 
-		
-		
 		System.out.println(tato.pelnoletnia());
 		System.out.println(corka.pelnoletnia());
 		System.out.println(tato.gigant());
 		System.out.println(corka.gigant());
-		
+
 		ArrayList<Osoba> dzieci = new ArrayList<>();
 		dzieci.add(corka);
 		dzieci.add(syn);
-		
+
 		Rodzina kowalscy = new Rodzina(mama, tato, dzieci);
 
 		Rodzina kowalscyBezDzieci = new Rodzina(mama, tato);
-		Rodzina kowalscyBezDzieciIMamy = new Rodzina(tato);
-		
-//		int lacznaWagaRodziny = kowalscy.lacznaWaga();
-//		int lacznaWagaRodziny = kowalscyBezDzieci.lacznaWaga();
-			
-		System.out.println("kowalscy "+kowalscy.lacznaWaga());
-		System.out.println("kowalscyBezDzieci "+kowalscyBezDzieci.lacznaWaga());
-		System.out.println("kowalscyBezDzieciIMamy "+kowalscyBezDzieciIMamy.lacznaWaga());
+		// Rodzina kowalscyBezDzieciIMamy = new Rodzina(tato);
+
+		// int lacznaWagaRodziny = kowalscy.lacznaWaga();
+		// int lacznaWagaRodziny = kowalscyBezDzieci.lacznaWaga();
+
+		System.out.println("kowalscy " + kowalscy.lacznaWaga());
+		System.out.println("Kobieta gigant " + mama.gigant());
+		System.out.println("Facet gigant " + tato.gigant());
+		System.out.println("kowalscyBezDzieci " + kowalscyBezDzieci.lacznaWaga());
+		// System.out.println("kowalscyBezDzieciIMamy " +
+		// kowalscyBezDzieciIMamy.lacznaWaga());
 	}
-// c
+	// c
 }
