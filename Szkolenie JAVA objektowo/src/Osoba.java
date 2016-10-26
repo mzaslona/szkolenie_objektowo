960aae7c04de555f3c844563f3856dc9af97dc8b
Student <Student@DESKTOP-U98QRUJ>
2016-10-26 14:45:59 +0200

    private

--- Szkolenie JAVA objektowo/src/Osoba.java
+++ Szkolenie JAVA objektowo/src/Osoba.java
@@ -3,12 +3,29 @@ public class Osoba {
 	String imie, nazwisko;
 	int wiek;
 	int wzrost;
-	int waga;
+	private int waga;
+	
+	
+	
+	public Osoba(String imie, String nazwisko, int wiek, int wzrost, int waga) {
+		super();
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.wiek = wiek;
+		this.wzrost = wzrost;
+		this.waga = waga;
+	}
+	
+	
 	boolean pelnoletnia() {
 		return wiek>= 18;
 	}
 	boolean gigant(){
 		return wzrost >180;
+	}
+	
+	int pobierzWagê(){
+		return waga;
 	}
 // c
 }
--- Szkolenie JAVA objektowo/src/OsobaMain.java
+++ Szkolenie JAVA objektowo/src/OsobaMain.java
@@ -3,33 +3,13 @@ import java.util.ArrayList;
 public class OsobaMain {
 
 	public static void main(String[] args) {
-		Osoba tato = new Osoba();
-		tato.imie = "Jan";
-		tato.nazwisko = "Kowalski";
-		tato.wiek = 35;
-		tato.wzrost =150;
-		tato.waga = 120;
 
-		Osoba mama = new Osoba();
-		mama.imie = "Julia";
-		mama.nazwisko = "Kowalska";
-		mama.wiek = 40;
-		mama.wzrost = 150;
-		mama.waga = 60;
+		Osoba mama = new Osoba("Halina", "Kowalska", 34, 150, 70);
+		Osoba tato = new Osoba("Jan", "Kowalski", 35, 180, 120);
+		Osoba corka = new Osoba("Bo¿ena", "Kowalska", 5, 120, 20);
+		Osoba syn = new Osoba("W³adzio", "Kowalski", 25, 210, 160);
+
 		
-		Osoba corka = new Osoba();
-		corka.imie = "Alicja";
-		corka.nazwisko = "Kowalska";
-		corka.wiek = 10;
-		corka.wzrost = 200;
-		corka.waga = 330;
-		
-		Osoba syn = new Osoba();
-		syn.imie = "Kuba";
-		syn.nazwisko = "Kowalski";
-		syn.wiek = 10;
-		syn.wzrost = 200;
-		syn.waga = 130;
 		
 		System.out.println(tato.pelnoletnia());
 		System.out.println(corka.pelnoletnia());
